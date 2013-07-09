ee1ed68d22d7b324c482de8cbca3b06196a6bec1
Betsy E. Cabrera <bcabrera97@gmail.com>
2013-07-09 16:49:59 -0400

    End of the day tuesday

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,6 +4,16 @@ require 'sinatra/reloader'
 secret_number= rand(101).to_s
 
 get "/" do 
-	
-	erb :index, :locals => {:number => secret_number}
+	 if secret_number.to_i > params["guess"].to_i
+	 	guess="too low"
+	 elsif secret_number.to_i < params["guess"].to_i
+	 	guess="too high!"
+	 elsif (secret_number.to_i)+5 <= params["guess"].to_i
+		guess="WAY to HIGH"
+	 elsif (secret_number.to_i)-5 >= params["guess"].to_i
+	 	guess="WAY to LOW"
+	 else secret_number.to_i == params["guess"].to_i
+	 	guess="CORECCT!"	
+	end
+	erb :index, :locals => {:number => secret_number, :guess => guess}
 end
