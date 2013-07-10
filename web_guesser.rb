edb877225099b93890e6b0da711f836b10117d50
Betsy E. Cabrera <bcabrera97@gmail.com>
2013-07-10 10:38:27 -0400

    Got way too high and way too low to work

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,16 +4,16 @@ require 'sinatra/reloader'
 secret_number= rand(101).to_s
 
 get "/" do 
-	 if secret_number.to_i > params["guess"].to_i
-	 	guess="too low"
-	 elsif secret_number.to_i < params["guess"].to_i
-	 	guess="too high!"
-	 elsif (secret_number.to_i)+5 <= params["guess"].to_i
-		guess="WAY to HIGH"
+	 if(secret_number.to_i)+5 <= params["guess"].to_i
+		guess="WAY too high!"
 	 elsif (secret_number.to_i)-5 >= params["guess"].to_i
-	 	guess="WAY to LOW"
+	 	guess="WAY too low!"
+	 elsif secret_number.to_i > params["guess"].to_i
+	 	guess="TOO low!"
+	 elsif secret_number.to_i < params["guess"].to_i
+	 	guess="TOO high!"
 	 else secret_number.to_i == params["guess"].to_i
-	 	guess="CORECCT!"	
+	 	guess="You got it right!"
 	end
 	erb :index, :locals => {:number => secret_number, :guess => guess}
 end
